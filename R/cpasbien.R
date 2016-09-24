fee421bffa998d00268675829aa7c78cd152323f
Romain Francois <romain@r-enthusiasts.com>
2016-09-24 19:42:51 +0200

    initial version

--- R/cpasbien.R
+++ R/cpasbien.R
@@ -1,18 +1,72 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Cmd + Shift + B'
-#   Check Package:             'Cmd + Shift + E'
-#   Test Package:              'Cmd + Shift + T'
+#' @export
+movies <- function( page = 1 ){
+  scrap( sprintf( "http://www.cpasbien.cm/view_cat.php?categorie=films&page=%d", page) )
+}
 
-hello <- function() {
-  print("Hello, world!")
+search <- function(query, page = 0, where = "" ){
+  query <- gsub( "[[:space:]]+", "-", query)
+  scrap( sprintf( "http://www.cpasbien.cm/recherche%s/%s/page-%d", where, query, page) )
+}
+
+#' @export
+search_movies <- function(...){
+  search(..., where = "/films")
+}
+
+#' @export
+search_episodes <- function(...){
+  search(..., where = "/series")
+}
+
+#' @export
+search_all <- function(...){
+  search(..., where = "")
+}
+
+
+#' @export
+episodes <- function( page = 1){
+  scrap( sprintf( "http://www.cpasbien.cm/view_cat.php?categorie=series&page=%d", page) )
+}
+
+#' @importFrom xml2 read_html
+#' @importFrom rvest html_nodes html_attr html_text
+#' @importFrom magrittr %>%
+#' @importFrom tibble data_frame
+#' @importFrom plyr llply
+scrap <- function(url){
+  html <- read_html(url)
+  data <- html %>% html_nodes( "#gauche a.titre" )
+  href <- data %>% html_attr("href")
+  titles <- data %>% html_text()
+
+  size <- html %>% html_nodes("#gauche .poid") %>% html_text()
+
+  data_frame( title = titles, href = href, size = size)
+}
+
+#' @importFrom dplyr bind_cols bind_rows
+#' @importFrom plyr llply
+#' @export
+details <- function(data, .progress = "text", ... ){
+  res <- llply( data$href, function(link){
+    html <- read_html(link)
+    torrent <- html %>%
+      html_nodes("a#telecharger") %>%
+      html_attr("href") %>%
+      sprintf( "http://www.cpasbien.cm%s", unlist(.) )
+
+    description <- html %>%
+      html_nodes("#textefiche p") %>%
+      tail(1) %>%
+      html_text
+
+    poster <- html %>%
+      html_nodes( "#bigcover img" ) %>%
+      html_attr("src")
+
+    data_frame( torrent = torrent, description = description, poster = poster)
+  }, .progress = .progress, ... )
+
+  bind_cols( data, bind_rows(res) )
 }
