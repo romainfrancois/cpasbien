3a7fa6fc41f54bd29cd02b5048c71f864b2bcd53
Romain Francois <romain@r-enthusiasts.com>
2016-09-25 14:34:25 +0200

    added process_movies

--- R/cpasbien.R
+++ R/cpasbien.R
@@ -1,3 +1,82 @@
+#' @importFrom dplyr if_else
+process_size <- function(size){
+  rx   <- "^([^[:space:]]*)[[:space:]]*([^[:space:]]*)[[:space:]]*$"
+  n    <- as.numeric(gsub(rx, "\\1", size ))
+  unit <- gsub(rx, "\\2", size )
+  round( ifelse( unit == "Mo", n, n*1024 ) )
+}
+
+#' @importFrom xml2 read_html
+#' @importFrom rvest html_nodes html_attr html_text
+#' @importFrom magrittr %>%
+#' @importFrom tibble data_frame
+#' @importFrom plyr llply
+scrap <- function(url){
+  html <- read_html(url)
+  data <- html %>% html_nodes( "#gauche a.titre" )
+  href <- data %>% html_attr("href")
+  titles <- data %>% html_text()
+
+  size <- html %>% html_nodes("#gauche .poid") %>% html_text() %>% process_size
+
+  data_frame( title = titles, href = href, size = size)
+}
+
+get_list <- function( page = 1, category ){
+  scrap( sprintf( "http://www.cpasbien.cm/view_cat.php?categorie=%s&page=%d", category, page) )
+}
+
+#' @importFrom xml2 read_html
+#' @importFrom rvest html_nodes
+#' @importFrom magrittr %>%
+npages <- function( category = "films" ){
+  url <- sprintf( "http://www.cpasbien.cm/view_cat.php?categorie=%s&page=1", category)
+  pages <- read_html(url) %>%
+    html_nodes("#pagination a") %>%
+    html_text()
+  as.numeric( pages[ length(pages) - 1] )
+}
+
+#' @importFrom plyr llply
+#' @importFrom dplyr bind_rows
+scrap_all <- function(category = "films", pages = seq_len(n), n = npages(category), .progress = "text", ... ){
+  res <- llply( pages, function(i){
+    get_list( page = i, category = category )
+  }, .progress = .progress, ...)
+  bind_rows(res)
+}
+
+
+#' @importFrom dplyr mutate select
+process_movies <- function(data){
+  rx      <- "^([^/]+)/(.*)[-]([0-9]{4})([-][0-9]*)?$"
+  lang_rx <- "^(.*)[-](vostfr|truefrench|french)(.*)$"
+
+  data %>%
+    mutate(
+      base     = gsub( "^.*/dl-torrent/films/(.*)[.]html$", "\\1", href ),
+      type     = gsub( rx, "\\1", base ),
+      middle   = gsub( rx, "\\2", base),
+      has_lang = grepl(lang_rx, middle),
+      title    = if_else(has_lang, gsub(lang_rx, "\\1", middle ), middle),
+      lang     = if_else(has_lang, gsub(lang_rx, "\\2", middle), NA_character_ ) ,
+      quality  = if_else(has_lang, gsub( "^[-]", "", gsub(lang_rx, "\\3", middle) ), NA_character_ ) ,
+      year     = gsub( rx, "\\3", base ),
+      base     = gsub( "^.*/", "", base ),
+      torrent  = sprintf( "http://www.cpasbien.cm/telechargement/%s.torrent", base )
+      ) %>%
+    select( type, title, year, lang, quality, size, torrent, url = href)
+
+}
+
+#' @export
+get_all_movies <- function( pages = seq_len(n), n = npages("films"), .progress = "text", ... ){
+  data <- scrap_all("films", pages = pages, .progress = .progress, ...)
+
+}
+
+
+
 
 search <- function(query, page = 0, where = "" ){
   query <- gsub( "[[:space:]]+", "-", query)
@@ -19,10 +98,6 @@ search_all <- function(...){
   search(..., where = "")
 }
 
-recent <- function( page = 1, category ){
-  scrap( sprintf( "http://www.cpasbien.cm/view_cat.php?categorie=%s&page=%d", category, page) )
-}
-
 process_episodes <- function( data ){
   rx <- "^(.*)S([[:digit:]]{2})E([[:digit:]]{2})(.*)$"
   res <- data  %>%
@@ -41,30 +116,13 @@ process_episodes <- function( data ){
 
 #' @importFrom dplyr filter
 #' @export
-all_episodes <- function( page = 1){
-  process_episodes( recent(page=page, category = "series") )
+get_episodes <- function( page = 1){
+  process_episodes( get_list(page=page, category = "series") )
 }
 
 #' @export
-all_movies <- function( page = 1 ){
-  recent(page=page, category = "films")
-}
-
-
-#' @importFrom xml2 read_html
-#' @importFrom rvest html_nodes html_attr html_text
-#' @importFrom magrittr %>%
-#' @importFrom tibble data_frame
-#' @importFrom plyr llply
-scrap <- function(url){
-  html <- read_html(url)
-  data <- html %>% html_nodes( "#gauche a.titre" )
-  href <- data %>% html_attr("href")
-  titles <- data %>% html_text()
-
-  size <- html %>% html_nodes("#gauche .poid") %>% html_text()
-
-  data_frame( title = titles, href = href, size = size)
+get_movies <- function( page = 1 ){
+  get_list(page=page, category = "films")
 }
 
 #' @importFrom dplyr bind_cols bind_rows
