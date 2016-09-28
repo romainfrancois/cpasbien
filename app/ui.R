7d456ec2b9af0011ba7766dd438f2bb34b2d5a8a
Romain Francois <romain@r-enthusiasts.com>
2016-09-28 16:06:44 +0200

    added purr sound

--- app/ui.R
+++ app/ui.R
@@ -4,7 +4,8 @@ shinyUI(fluidPage(
 
   absolutePanel(
     bottom = "10px", right = "10px", width = "150px", height = "150px" , style = "z-index: -2",
-    img( src = "purrple.png" , width = "150px", height = "150px")
+    img( src = "purrple.png" , width = "150px", height = "150px", id = "purrple",
+         onmouseover = '{ var audio = new Audio("purr.mp3");  audio.play() ; }')
   ),
   sidebarPanel(
     htmlOutput( "movie_description")
